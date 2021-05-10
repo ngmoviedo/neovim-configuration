b39d308de4416b2e0ab1b2e7e1aba3f5b3b4bfb7
nicolas <ngmoviedo@gmx.com>
2021-05-11 01:18:43 +0200

    Removed redundant line from pandoc ftplugin

--- ftplugin/pandoc.lua
+++ ftplugin/pandoc.lua
@@ -1,5 +1,3 @@
 -- Spelling
--- vim.api.nvim_win_set_option(0, 'spell', true)
-vim.cmd('setlocal spell')
 vim.g['pandoc#spell#default_langs'] = "es" -- Not working in lua
 vim.api.nvim_buf_set_option(0, 'spelllang', 'es') -- Needs this instead
