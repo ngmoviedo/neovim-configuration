cabea726f767432cba9e51d6fa8b1c0199d6a716
nicolas <ngmoviedo@gmx.com>
2021-05-12 01:43:21 +0200

    LSP keymaps. Copied from nvim-lspconfig docs

--- lua/plugin/treesitter.lua
+++ lua/plugin/treesitter.lua
@@ -1,15 +1,13 @@
 ﻿require'nvim-treesitter.configs'.setup {
-	ensure_installed = { "bash","lua","latex","python","css","typescript","yaml" },
-	highlight = {
-		enable = true,
-        disable = {"latex"}, -- no highlighting for latex
-	},
-	indent = {
-		enable = true
-	},
-	autopairs = {
-		enable = true
-	}
+    ensure_installed = {
+        "bash", "lua", "latex", "python", "css", "typescript", "yaml"
+    },
+    highlight = {
+        enable = true,
+        disable = {"latex"} -- no highlighting for latex
+    },
+    indent = {enable = true},
+    autopairs = {enable = true}
 }
 
 -- Vimade compatibility (experimental)
