94325fe70105796f7efdac8d1e5d68193a2b3dd1
nicolas <ngmoviedo@gmx.com>
2021-05-13 13:00:50 +0200

    Improved pairing rules

--- lua/plugin/autopairs.lua
+++ lua/plugin/autopairs.lua
@@ -15,12 +15,24 @@ npairs.get_rule('{'):with_pair(cond.not_before_regex_check("\\"))
 
 --- Specific pairs
 npairs.add_rules({
-    Rule("$", "$", {"tex", "latex", "pandoc", "markdown"}),
-    Rule('\\left(', '\\right)', {"tex", "latex"}),
-    Rule('\\left[', '\\right]', {"tex", "latex"}),
-    Rule('\\left\\{', '\\right\\}', {"tex", "latex"}),
-    Rule('\\left\\langle', '\\right\\rangle', {"tex", "latex"}),
-    Rule('\\langle', '\\rangle', {"tex", "latex"}),
-    Rule('\\|', '\\|', {"tex", "latex"}),
+    Rule("$", "$", {"tex", "latex", "pandoc", "markdown"})
+        :with_pair(cond.not_before_regex_check("\\"))
+        :with_pair(cond.not_before_regex_check("[%w%.]"))
+        :with_pair(cond.not_after_regex_check("[%w%.]")),
+    Rule('\\left(', '\\right)', {"tex", "latex"})
+        :with_pair(cond.not_after_regex_check("[%w%.]")),
+    Rule('\\left[', '\\right]', {"tex", "latex"})
+        :with_pair(cond.not_after_regex_check("[%w%.]")),
+    Rule('\\left\\{', '\\right\\}', {"tex", "latex"})
+        :with_pair(cond.not_after_regex_check("[%w%.]")),
+    Rule('\\left\\langle', '\\right\\rangle', {"tex", "latex"})
+        :with_pair(cond.not_after_regex_check("[%w%.]")),
+    Rule('\\langle', '\\rangle', {"tex", "latex"})
+        :with_pair(cond.not_after_regex_check("[%w%.]")),
+    Rule('\\|', '\\|', {"tex", "latex"})
+        :with_pair(cond.not_after_regex_check("[%w%.]")),
     Rule('*', '*', {"pandoc", "markdown"})
+        :with_pair(cond.not_before_regex_check("\\"))
+        :with_pair(cond.not_before_regex_check("[%w%.]"))
+        :with_pair(cond.not_after_regex_check("[%w%.]")),
 })
